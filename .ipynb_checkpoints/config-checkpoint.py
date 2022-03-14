7007b53845698b374b0fb8d0f786281bd390a3a8
Michael Keith <michaelkeith@utah.gov>
2022-03-14 13:07:10 -0600

    Made fcst_length configurable
    Felt like the right thing to do

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -1,2 +1,3 @@
-metric = 'MAPE'
-train_prop = .7
+metric = 'MAPE' # metric used to compare all results (MAE, RMSE, R2 also available)
+train_prop = .7 # the size of the train split on each series
+fcst_length = 38 # the size of the forecast into future periods
